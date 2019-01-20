ad934302332ca560f1c3e5f9b9bac1c82a9248fb
Evan Martinez <evanm.design@gmail.com>
2019-01-20 06:10:37 -0500

    transaction route

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var request = require('request');
 var cors = require('cors');
 
 var app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/lightning-status', function (request, response) {
 	fetchStatus(response);
 });
 
+app.post('/initiate-transaction', function(req, res) {
+	console.log('got', req.body);
+	var options = {
+		url: 'https://localhost:8080/v1/transactions',
+		rejectUnauthorized: false,
+		json: true,
+		headers: {
+			'Grpc-Metadata-macaroon': macaroon,
+		},
+		form: JSON.stringify(req.body),
+	};
+	request.post(options, function(err, response, body) {
+		res.json(body);
+	});
+});
+
 function fetchStatus(res) {
-	var macaroon = fs.readFileSync('/home/pi/admin.macaroon').toString('hex');
+	var macaroon = getMacaroon();
 	var options = {
 		url: 'https://localhost:8080/v1/getinfo',
 		rejectUnauthorized: false,
@@ -24,6 +43,10 @@ function fetchStatus(res) {
 	request.get(options, function(err, response, body) {
 		res.json(body);
 	});
+}
+
+function getMacaroon() {
+	return fs.readFileSync('/home/pi/admin.macaroon').toString('hex');
 }
 
 app.listen('4200', function() {
